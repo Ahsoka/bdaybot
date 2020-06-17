44bcf396f98246ce385fb49a4919df4ba604596a
Andres Torres <34612646+TurretAA12@users.noreply.github.com>
2020-06-16 17:46:22 -0700

    Fixed another typo in the assert statement

--- bday.py
+++ bday.py
@@ -10,7 +10,7 @@ bot = commands.Bot(command_prefix = "!", description = description)
 # Use enviroment variables to access the Discord token so it's not visible in the code
 TOKEN = os.environ.get('Bday_Token')
 assert TOKEN is not None, ("The Discord token could not be found in environment variables.\n"
-                        "See this video on how add the url to the environment variables (name the enviroment variable 'bday_data_URL' without quotes): "
+                        "See this video on how add the url to the environment variables (name the enviroment variable 'Bday_Token' without quotes): "
                         f"{data.windows_vid_url}" if 'nt' in os.name else f"{data.unix_vid_url}")
 
 def update():
