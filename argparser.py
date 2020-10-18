c7deed3ed8797a678fe3acb107513cdcc30d4ad9
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-10-18 15:33:13 -0700

    Changed default database in argparser.py

--- argparser.py
+++ argparser.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Use to set various settings of the bdaybot on runtime.")
 
-parser.add_argument('-db', '--database', default='bdaybot.db')
+parser.add_argument('-db', '--database', default=':memory:')
 parser.add_argument('-nt', '--not-testing', action='store_false', dest='testing')
 
 args = parser.parse_args()
