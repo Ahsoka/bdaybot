efb6d67d4e44f4532afd007542672b1c79d81679
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-10-18 20:28:07 -0700

    Created a basic testing framework

---
+++ tests/test_bdaybot.py
@@ -0,0 +1,24 @@
+import pathlib
+import sys
+two_levels_up = pathlib.Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(two_levels_up))
+
+import unittest
+from create_database import create_guilds_table, create_discord_users_table
+
+class TestBdaybot(unittest.TestCase):
+    BDAY_SERVER_ROLE_ID = 767587634796429374
+    STARSHIP_SERVER_ROLE_ID = 767552973948583966
+
+    @classmethod
+    def setUpClass(cls):
+        connection = sqlite3.connect(args.database, detect_types=sqlite3.PARSE_DECLTYPES)
+        # Automatically convert 0 or 1 to bool
+        sqlite3.register_converter("BOOLEAN", lambda val: bool(int(val)))
+        # DEBUG: **MUST** include this line in order to use
+        # FOREIGN KEYS, by default they are **DISABLED**
+        connection.execute("PRAGMA foreign_keys = 1")
+        cursor = connection.cursor()
+        with connection:
+            cursor.execute('\n'.join(create_discord_users_table.splitlines()[:-2])[:-1] + ')')
+            cursor.execute(create_guilds_table)
