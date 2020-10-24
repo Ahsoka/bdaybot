99d81f7287d433f186afbfbb796bb4fdcc396e5f
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-10-24 14:31:50 -0700

    Added BDAY_SERVER_ID, STARSHIP_SERVER_ID, and TESTING_CHANNEL_ID values

--- tests/test_bdaybot.py
+++ tests/test_bdaybot.py
@@ -13,8 +13,13 @@ parser.add_argument('-db', '--database', default=':memory:',
                     help="Use this to set the SQLite3 database location. (default: %(default)s)")
 
 class TestBdaybot(unittest.TestCase):
+    BDAY_SERVER_ID = 713095060652163113
+    STARSHIP_SERVER_ID = 675806001231822863
+
     BDAY_SERVER_ROLE_ID = 767587634796429374
     STARSHIP_SERVER_ROLE_ID = 767552973948583966
+
+    TESTING_CHANNEL_ID = 769671372963971072
 
     @classmethod
     def setUpClass(cls):
