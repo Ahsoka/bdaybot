90714b50b78e0667a4006d499bb04d796e6785e2
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-11-12 20:34:42 -0800

    Added ASIN argument to argparse

--- argparser.py
+++ argparser.py
@@ -1,8 +1,10 @@
+import os
 import argparse
 
 parser = argparse.ArgumentParser(description="Use to set various settings of the bdaybot on runtime.")
 
 parser.add_argument('-db', '--database', default=':memory:')
 parser.add_argument('-nt', '--not-testing', action='store_false', dest='testing')
+parser.add_argument('-a', '--ASIN', default=os.environ['ASIN'])
 
 args = parser.parse_args()
