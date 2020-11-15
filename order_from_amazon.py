e8e2bced9bd8c95fa0e12955b9b4e5679a37803f
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-11-14 19:22:33 -0800

    Change driver logging location from logs/chrome-logs to logs/chorme.log

--- order_from_amazon.py
+++ order_from_amazon.py
@@ -49,7 +49,7 @@ def order_product(ASIN,
             chrome_options.add_argument('--headless'); # chrome_options.add_argument('--no-sandbox')
             driver = webdriver.Chrome(executable_path=str(pathlib.Path('chrome86-driver').resolve()),
                                       chrome_options=chrome_options,
-                                      service_args=['--verbose', f"--log-path={pathlib.Path('logs/chrome-logs').resolve()}"])
+                                      service_args=['--verbose', f"--log-path={pathlib.Path('logs/chrome.log').resolve()}"])
             driver.set_window_size(width=1363, height=1094)
         else:
             driver = webdriver.Chrome('chrome86-driver.exe')
