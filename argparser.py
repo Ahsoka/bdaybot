2cce50a33e8e5db3c7a94ab7dbb033f331b5e226
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-11-17 14:20:04 -0800

    Changed default database to test.db
    (this parameter is only active when testing the bot)

--- argparser.py
+++ argparser.py
@@ -3,7 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Use to set various settings of the bdaybot on runtime.")
 
-parser.add_argument('-db', '--database', default=':memory:')
+parser.add_argument('-db', '--database', default='test.db')
 parser.add_argument('-nt', '--not-testing', action='store_false', dest='testing')
 parser.add_argument('-a', '--ASIN', default=os.environ['ASIN'])
 parser.add_argument('-po', '--place-order', action='store_true', dest='place_order')
