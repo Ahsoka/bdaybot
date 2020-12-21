77ef94f0a84d61d8141c019f65d56271997fcc84
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-12-21 15:51:34 -0800

    Added token as a parameter in argparser

--- bdaybot/argparser.py
+++ bdaybot/argparser.py
@@ -8,3 +8,4 @@ parser.add_argument('-nt', '--not-testing', action='store_false', dest='testing'
 parser.add_argument('-a', '--ASIN', default=os.environ['ASIN'])
 parser.add_argument('-po', '--place-order', action='store_true', dest='place_order')
 parser.add_argument('-pe', '--print-envelope', action='store_true', dest='print_envelope')
+parser.add_argument('-t', '--token')
