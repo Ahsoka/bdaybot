0f613e661c0aa08ad9653438006f1b438033e821
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-12-23 13:37:15 -0800

    Removed default ASIN value from argparser.py

--- bdaybot/argparser.py
+++ bdaybot/argparser.py
@@ -1,11 +1,10 @@
-import os
 import argparse
 
 parser = argparse.ArgumentParser(description="Use to set various settings of the bdaybot on runtime.")
 
 parser.add_argument('-db', '--database', default='sqlite:///:memory:')
 parser.add_argument('-nt', '--not-testing', action='store_false', dest='testing')
-parser.add_argument('-a', '--ASIN', default=os.environ['ASIN'])
+parser.add_argument('-a', '--ASIN')
 parser.add_argument('-po', '--place-order', action='store_true', dest='place_order')
 parser.add_argument('-pe', '--print-envelope', action='store_true', dest='print_envelope')
 parser.add_argument('-t', '--token')
