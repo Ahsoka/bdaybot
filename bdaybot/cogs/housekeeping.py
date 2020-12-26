417f476ee28c90f1d2798d0dadf15a0ded99f9ef
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2020-12-25 16:36:27 -0800

    Fixed invalid reference to super() in channel.mention()

--- bdaybot/cogs/housekeeping.py
+++ bdaybot/cogs/housekeeping.py
@@ -13,7 +13,8 @@ logger = logging.getLogger(__name__)
 def mention(self):
     # NOTE: Keep an eye on Discord mobile because they might change it
     # so it does not always say '#invalid-channel' and actually shows the channel
-    return f'**#{channel}**' if self.guild.owner and self.guild.owner.is_on_mobile() else super().mention
+    return f'**#{channel}**' if self.guild.owner and self.guild.owner.is_on_mobile() \
+           else f'<#{self.id}>'
 
 discord.TextChannel.mention = mention
 
