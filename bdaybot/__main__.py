345b58dbdf80d527e5209fb8f9e1ab258bc0656e
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-01-27 12:09:59 -0800

    Removed + as a command prefix

--- bdaybot/__main__.py
+++ bdaybot/__main__.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 config.andres = True
 
-the_bot = bdaybot(command_prefix='test.' if config.testing else ('b.', '+'),
+the_bot = bdaybot(command_prefix='test.' if config.testing else 'b.',
                   case_insensitive=True)
 logger_names = [
     'bdaybot.data',
@@ -23,6 +23,5 @@ for logger_name in logger_names:
     setUpLogger(logger_name,
                 files=not config.testing,
                 fmt='[%(levelname)s] %(name)s: %(asctime)s - [%(funcName)s()] %(message)s')
-
 
 the_bot.run(os.environ['testing_token'] if config.testing else os.environ['Bday_Token'])
