bf23b47927224e5e025bae12b5c478e24626fcfc
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-03-27 20:53:53 -0700

    Changed default SQLite connection string to use aiosqlite

--- bdaybot/argparser.py
+++ bdaybot/argparser.py
@@ -7,7 +7,7 @@ class IgnoreUnrecognizedArgs(argparse.ArgumentParser):
 parser = IgnoreUnrecognizedArgs(description="Use to set various settings of the bdaybot on runtime.",
                                 add_help=False)
 
-parser.add_argument('-db', '--database', default='sqlite:///:memory:')
+parser.add_argument('-db', '--database', default='sqlite+aiosqlite:///:memory:')
 parser.add_argument('-nt', '--not-testing', action='store_false', dest='testing')
 parser.add_argument('-a', '--ASIN')
 parser.add_argument('-po', '--place-order', action='store_true', dest='place_order')
