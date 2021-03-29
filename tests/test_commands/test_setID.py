280c09176a250d8b6619e78bf701e2520041513b
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-03-28 17:27:51 -0700

    Slight improvement to test_setID unit test

--- tests/test_commands/test_setID.py
+++ tests/test_commands/test_setID.py
@@ -31,6 +31,7 @@ async def test_setID(bot, session, valid_ids, channel, mock_delete, dashes, dela
     discord.message.delete_message.assert_awaited_with(message)
     # Check to see if the ID is properly added to the database
     the_bot = await session.get(DiscordUser, bot.user.id)
+    assert the_bot, "The bot was not found in the database"
     assert the_bot.student_id == valid_id, \
            f"Bot's ID is {the_bot.student_id} it's supposed to be {valid_id}" if the_bot else \
            "the_bot is None"
