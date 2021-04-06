d1a2512785d2c055a829a69ff6d3e3f6ecedaf9d
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-04-06 11:45:33 -0700

    Added a way to backup the PostgreSQL database to an SQLite database
    This closes #26

---
+++ backup.py
@@ -0,0 +1,24 @@
+import argparse
+
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+from bdaybot.tables import Base
+from bdaybot import postgres_URL
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-b', '--backup', default='sqlite:///backup.db')
+
+config = parser.parse_args()
+
+sqlite_engine = create_engine(config.backup, echo=True)
+postgres_engine = create_engine(postgres_URL.set(drivername='postgresql+psycopg2'))
+
+with sqlite_engine.begin() as conn:
+    Base.metadata.create_all(conn)
+
+with sqlite_engine.begin() as sqlite, postgres_engine.connect() as postgres:
+    for table in Base.metadata.sorted_tables:
+        params = [dict(row) for row in postgres.execute(select(table.c))]
+        print(params)
+        sqlite.execute(table.insert(), params)
